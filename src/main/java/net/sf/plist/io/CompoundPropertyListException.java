4ae1b3ecab019577f619e9dd0aad9b514c873205
yorn <git@yorn.priv.no>
2012-08-10 11:41:12 +0000

    Fixed javadoc
    git-svn-id: svn://svn.code.sf.net/p/plist/code/trunk@91 b73d6b93-3bbc-4436-bb8c-8d7da4af9efb

--- src/main/java/net/sf/plist/io/CompoundPropertyListException.java
+++ src/main/java/net/sf/plist/io/CompoundPropertyListException.java
@@ -1,6 +1,24 @@
-/**
- * 
- */
+/*
+Property List Dictionary - LGPL 3.0 licensed
+Copyright (C) 2012  Yørn de Jong
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+File is part of the Property List project.
+Project page on http://plist.sf.net/
+*/
 package net.sf.plist.io;
 
 import java.util.Arrays;
@@ -9,22 +27,29 @@ import java.util.List;
 
 
 /**
- * @author yorn
- *
+ * If multiple operations with a Property List failed,
+ * all exceptions that occurred can be packed inside this one exception.
  */
 public class CompoundPropertyListException extends PropertyListException {
 	private static final long serialVersionUID = 1L;
 
+	/** The exceptions that occurred */
 	protected PropertyListException[] why;
 	
 	/**
-	 * @param reason
+	 * Construct a new CompoundPropertyListException
+	 * @param why	All exceptions that occurred when trying to complete an operation
 	 */
 	public CompoundPropertyListException(PropertyListException... why) {
 		super(getReason(why));
 		this.why = why;
 	}
-
+	
+	/**
+	 * Construct a reason string from all given exceptions
+	 * @param why	All exceptions that occurred when trying to complete an operation
+	 * @return	A human-readable reason
+	 */
 	private static String getReason(PropertyListException... why) {
 		String result = "\n";
 		for(int i=0;i<why.length;i++)
@@ -32,6 +57,10 @@ public class CompoundPropertyListException extends PropertyListException {
 		return result;
 	}
 	
+	/**
+	 * Return a list of all reasons this exception was instantiated
+	 * @return	the reasons
+	 */
 	public List<? extends PropertyListException> getAllReasons() {
 		return Collections.unmodifiableList(Arrays.asList(why));
 	}
